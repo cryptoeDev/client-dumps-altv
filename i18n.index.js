413f6d1886c149a5cdab00f636a32be68ca27d09
cryptoe <wfrocket.srv001@yandex.ru>
2026-06-19 00:53:49 +0500

    Add files via upload

---
+++ i18n.index.js
@@ -0,0 +1,134 @@
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{};e.SENTRY_RELEASE={id:"5d2f893b5b145179dd858efb1c705979956d4792"};var n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="8985f626-716b-4a89-a649-8c195bb0077a",e._sentryDebugIdIdentifier="sentry-dbid-8985f626-716b-4a89-a649-8c195bb0077a");}catch(e){}}();
+"use strict";
+exports.id = 545;
+exports.ids = [545];
+exports.modules = {
+
+/***/ 61851
+(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   a: () => (/* binding */ languagesConf)
+/* harmony export */ });
+/* unused harmony export importMap */
+/* harmony import */ var _de_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(36796);
+/* harmony import */ var _en_json__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(51816);
+/* harmony import */ var _es_json__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(77199);
+/* harmony import */ var _pl_json__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(75459);
+/* harmony import */ var _pt_json__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(53867);
+/* harmony import */ var _ru_json__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(76162);
+/* harmony import */ var _uk_json__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(37927);
+
+
+
+
+
+
+
+
+const languagesConf = { de: _de_json__WEBPACK_IMPORTED_MODULE_0__, en: _en_json__WEBPACK_IMPORTED_MODULE_1__, es: _es_json__WEBPACK_IMPORTED_MODULE_2__, pl: _pl_json__WEBPACK_IMPORTED_MODULE_3__, pt: _pt_json__WEBPACK_IMPORTED_MODULE_4__, ru: _ru_json__WEBPACK_IMPORTED_MODULE_5__, uk: _uk_json__WEBPACK_IMPORTED_MODULE_6__ };
+
+const importMap = {
+  de: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 36796, 17)),
+  en: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 51816, 17)),
+  es: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 77199, 17)),
+  pl: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 75459, 17)),
+  pt: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 53867, 17)),
+  ru: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 76162, 17)),
+  uk: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 37927, 17)),
+};
+
+
+/***/ },
+
+/***/ 55071
+(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   a: () => (/* binding */ languagesConf)
+/* harmony export */ });
+/* harmony import */ var _de_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(28682);
+/* harmony import */ var _en_json__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(34114);
+/* harmony import */ var _es_json__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(92833);
+/* harmony import */ var _pl_json__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(43489);
+/* harmony import */ var _pt_json__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(35577);
+/* harmony import */ var _ru_json__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(41188);
+/* harmony import */ var _uk_json__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(98537);
+
+
+
+
+
+
+
+
+const languagesConf = { de: _de_json__WEBPACK_IMPORTED_MODULE_0__, en: _en_json__WEBPACK_IMPORTED_MODULE_1__, es: _es_json__WEBPACK_IMPORTED_MODULE_2__, pl: _pl_json__WEBPACK_IMPORTED_MODULE_3__, pt: _pt_json__WEBPACK_IMPORTED_MODULE_4__, ru: _ru_json__WEBPACK_IMPORTED_MODULE_5__, uk: _uk_json__WEBPACK_IMPORTED_MODULE_6__ };
+
+
+/***/ },
+
+/***/ 31539
+(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   a: () => (/* binding */ languagesConf)
+/* harmony export */ });
+/* unused harmony export importMap */
+/* harmony import */ var _de_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(40192);
+/* harmony import */ var _en_json__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(51228);
+/* harmony import */ var _es_json__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(65387);
+/* harmony import */ var _pl_json__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(25855);
+/* harmony import */ var _pt_json__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(32999);
+/* harmony import */ var _ru_json__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(42478);
+/* harmony import */ var _uk_json__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(79187);
+
+
+
+
+
+
+
+
+const languagesConf = { de: _de_json__WEBPACK_IMPORTED_MODULE_0__, en: _en_json__WEBPACK_IMPORTED_MODULE_1__, es: _es_json__WEBPACK_IMPORTED_MODULE_2__, pl: _pl_json__WEBPACK_IMPORTED_MODULE_3__, pt: _pt_json__WEBPACK_IMPORTED_MODULE_4__, ru: _ru_json__WEBPACK_IMPORTED_MODULE_5__, uk: _uk_json__WEBPACK_IMPORTED_MODULE_6__ };
+
+const importMap = {
+    de: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 40192, 17)),
+    en: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 51228, 17)),
+    es: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 65387, 17)),
+    pl: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 25855, 17)),
+    pt: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 32999, 17)),
+    ru: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 42478, 17)),
+    uk: () => Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 79187, 17)),
+};
+
+
+/***/ },
+
+/***/ 2301
+(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   a: () => (/* binding */ languagesConf)
+/* harmony export */ });
+/* harmony import */ var _de_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(3760);
+/* harmony import */ var _en_json__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(15116);
+/* harmony import */ var _es_json__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(11611);
+/* harmony import */ var _pl_json__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(37679);
+/* harmony import */ var _pt_json__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(71511);
+/* harmony import */ var _ru_json__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(34366);
+/* harmony import */ var _uk_json__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(94211);
+
+
+
+
+
+
+
+
+const languagesConf = { de: _de_json__WEBPACK_IMPORTED_MODULE_0__, en: _en_json__WEBPACK_IMPORTED_MODULE_1__, es: _es_json__WEBPACK_IMPORTED_MODULE_2__, pl: _pl_json__WEBPACK_IMPORTED_MODULE_3__, pt: _pt_json__WEBPACK_IMPORTED_MODULE_4__, ru: _ru_json__WEBPACK_IMPORTED_MODULE_5__, uk: _uk_json__WEBPACK_IMPORTED_MODULE_6__ };
+
+
+/***/ }
+
+};
+;
